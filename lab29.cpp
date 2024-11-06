5378471a10d6b2d4287b45a6b1c671d52ba5e5c9
Skylar Robinson <skylardawn150@gmail.com>
2024-11-05 17:39:26 -0800

    Commit 1

--- lab29.cpp
+++ lab29.cpp
@@ -3,3 +3,33 @@
 COMSC 210 | Lab 29 | Skylar Robinson | IDE Used: Eclipse
 
 *********************************************************/
+
+//headers
+
+//function prototypes
+//random name function
+	//no parameters, returns string
+//one time cycle function
+	//parameters: map of drinks by reference
+
+//define constants
+	//time cycle, max drinks ordered, min drinks ordered, max drinks served
+
+//define main function
+	//initialize map with string keys and array of 3 lists value
+
+	//open file of drink names
+		//check for file open error
+
+	//populate map keys with drink names
+	//add an initial 1 name to each ordered list
+
+	//close file
+
+
+
+
+
+
+
+
