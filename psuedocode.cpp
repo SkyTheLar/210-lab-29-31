e4294a4e168c035e93a2669c8589b10b5c3e62d9
Skylar Robinson <skylardawn150@gmail.com>
2024-11-05 18:19:34 -0800

    Commit 5

--- psuedocode.cpp
+++ psuedocode.cpp
@@ -5,16 +5,10 @@ COMSC 210 | Lab 29 | Skylar Robinson | IDE Used: Eclipse
 *********************************************************/
 
 //headers
-#include <iostream>
-#include <array>
-#include <list>
-#include <map>
-using namespace std;
 
 //define constants
 	//time intervals, max drinks ordered,
 	//max drinks served, number of names, number of drinks
-int const INTERVALS = 48, MAX_OR = 10, MAX_SV = 10, NAMES = 99, DRINKS = 41;
 
 //function prototypes
 //random name function
@@ -23,7 +17,6 @@ int const INTERVALS = 48, MAX_OR = 10, MAX_SV = 10, NAMES = 99, DRINKS = 41;
 	//parameters: map of drinks by reference, array of keys; returns: nothing
 
 //define main function
-int main() {
 	//initialize map with string keys and array of 3 lists value
 	//initialize array for names
 	//initialize array for drink names
@@ -50,9 +43,7 @@ int main() {
 	//end simulation
 		//display final map data
 
-	return 0;
 //end main function
-}
 
 //random name function
 	//select random index from 0 to names - 1
