a309e45d2d20cb48e534cef16917778fce50e3f2
Skylar Robinson <skylardawn150@gmail.com>
2024-11-05 17:49:28 -0800

    Commit 2

--- lab29.cpp
+++ lab29.cpp
@@ -6,30 +6,51 @@ COMSC 210 | Lab 29 | Skylar Robinson | IDE Used: Eclipse
 
 //headers
 
+//define constants
+	//time intervals, max drinks ordered, min drinks ordered,
+	//max drinks served, number of names, number of drinks
+
 //function prototypes
 //random name function
-	//no parameters, returns string
+	//parameters: array of size names; returns: string
 //one time cycle function
-	//parameters: map of drinks by reference
-
-//define constants
-	//time cycle, max drinks ordered, min drinks ordered, max drinks served
+	//parameters: map of drinks by reference, , returns nothing
 
 //define main function
 	//initialize map with string keys and array of 3 lists value
+	//initialize array for names
+	//initialize array for drink names
+
+	//open people names file
+		//check for file open error
+
+	//read names into names array
+
+	//close file
 
 	//open file of drink names
 		//check for file open error
 
 	//populate map keys with drink names
+	//populate drink names array with same data
 	//add an initial 1 name to each ordered list
 
 	//close file
 
+	//begin simulation
+		//loop through simulation function for 48 intervals
 
+	//end simulation
+		//display final map data
 
+//end main function
 
+//random name function
+	//select random index from 0 to names - 1
+	//return that name in passed array
 
+//time simulation function
+	//choose random number of drinks to be ordered
 
 
 
