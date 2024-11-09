86ab34ee758c97f61e6ef169f76093912e633fb2
Skylar Robinson <skylardawn150@gmail.com>
2024-11-08 16:05:08 -0800

    Commit 6

--- lab30.cpp
+++ lab30.cpp
@@ -17,6 +17,7 @@ using namespace std;
 int const INTERVALS = 48, MAX_OR = 10, MAX_SV = 10, NAMES = 3, DRINKS = 5;
 
 string getName(string[]); //random name function
+int numMade(); //random number increasingly less likely to be higher
 void timeCycle(map<string, array<list<string>, 3>>&, string[], string[]); //one time cycle function
 
 int main() {
@@ -77,6 +78,16 @@ int main() {
 	}
 	cout << "\n\n";
 
+	timeCycle(cafe, drinks, names);
+
+	for (auto pair : cafe) {
+			cout << "Drink: " << pair.first
+				 << "; Ordered: ";
+			for (string n : pair.second[0])
+				cout << n << " ";
+			cout << endl;
+		}
+		cout << "\n\n";
 
 	//begin simulation
 		//loop through simulation function for 48 intervals
@@ -96,6 +107,11 @@ string getName(string names[]) {
 	return names[ind];
 }
 
+//random made number functon
+int numMade() {
+
+}
+
 //time simulation function
 void timeCycle(map<string, array<list<string>, 3>> &cafe, string drinks[], string names[]) {
 	//test moving name from ordered to made
@@ -111,7 +127,7 @@ void timeCycle(map<string, array<list<string>, 3>> &cafe, string drinks[], strin
 
 	//beginning of interval flag
 	static int counter = 1;
-	cout << "Time interval " << counter;
+	cout << "Time interval " << counter << ":\n\n";
 
 	//choose random number of drinks to be ordered
 	int ordered = (rand() % MAX_OR) + 1;
@@ -121,29 +137,34 @@ void timeCycle(map<string, array<list<string>, 3>> &cafe, string drinks[], strin
 		string temp = getName(names);
 		//random index from drink array
 		auto it = cafe.find(drinks[rand() % DRINKS]);
-		//add random name to map item with key from drink array
 		if (it == cafe.end()) {
 			cout << "Range error.\n";
 			return;
 		}
+		//add random name to map item with key from drink array
+		it->second[0].push_back(temp);
 
 		//display names and drinks ordered
-			//"Hannah ordered Coffee"
+		cout << temp << " ordered " << it->first << endl;
 	}
 
 	//loop for each map element
+	for (auto it = cafe.begin(); it != cafe.end(); it++) {
 		//random number drinks to be made
 		//move that many names from the front of ordered to the back of made
 		//display drinks made for each drink on one line
 			//"Mocha was made for: Cassie, Lena, Joe"
+	}
 
 	//loop for each map element
+	for (auto it = cafe.begin(); it != cafe.end(); it++) {
 		//if made.length >= max drinks served
 			//serve that many drinks
 		//else
 			//serve all the drinks
 		//display drinks served for each drink on one line
 			//"Mocha was served to: Cassie, Lena, Joe, Tim"
+	}
 
 	counter++;
 }
